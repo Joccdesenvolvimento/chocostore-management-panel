a6164001821619afeebf8aeaff66f790b8ec6cb8
desenvolvimento02jocc <desenvolvimento02@jocc.com.br>
2022-09-08 16:30:07 -0300

    base commit

--- src/plugins/external-components.js
+++ src/plugins/external-components.js
@@ -1,8 +1,8 @@
 /* Url plugin: https://vuejsexamples.com/toggle-switch-for-vue-js/ */
 import Vue from 'vue';
-import ToggleSwitch from 'vuejs-toggle-switch';
+/* import ToggleSwitch from 'vuejs-toggle-switch'; */
 import VueMask from 'v-mask';
 // import money from 'v-money'
-Vue.use(ToggleSwitch);
+/* Vue.use(ToggleSwitch); */
 Vue.use(VueMask);
 // Vue.use(money, {precision: 2})
--- src/utils/base-url-apiapp.js
+++ src/utils/base-url-apiapp.js
@@ -1,4 +1,4 @@
-const baseURL = 'http://192.168.0.45:3010';
+const baseURL = 'http://chocostore.ddns.net:3010';
 //const baseURL = 'http://189.7.137.34:3000';
 
 export default baseURL;
